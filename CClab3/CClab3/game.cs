cdd756d013c93474e0b5991b911be0db6fd93d48
JMAPeters <jorn.peters@live.nl>
2019-01-17 17:16:46 +0100

    idk

--- CClab3/CClab3/game.cs
+++ CClab3/CClab3/game.cs
@@ -19,9 +19,9 @@ namespace Template
         // find the kernel named 'device_function' in the program
         OpenCLKernel kernel = new OpenCLKernel( ocl, "device_function" );
         // create a regular buffer; by default this resides on both the host and the device
-        OpenCLBuffer<uint> Sbuffer;
+        OpenCLBuffer<int> buffer = new OpenCLBuffer<int>( ocl, 512 * 512 );
         // create an OpenGL texture to which OpenCL can send data
-        //OpenCLImage<int> image = new OpenCLImage<int>( ocl, 512, 512 );
+        OpenCLImage<int> image = new OpenCLImage<int>( ocl, 512, 512 );
 
         //COPYED FROM CPU VERSION
         // screen surface to draw to
@@ -30,24 +30,45 @@ namespace Template
         Stopwatch timer = new Stopwatch();
         int generation = 0;
         // two buffers for the pattern: simulate reads 'second', writes to 'pattern'
-        static uint[] pattern;
-        static uint[] second;
+        uint[] pattern;
+        uint[] second;
         uint pw, ph; // note: pw is in uints; width in bits is 32 this value.
-
+        // helper function for setting one bit in the pattern buffer
+        void BitSet(uint x, uint y) { pattern[y * pw + (x >> 5)] |= 1U << (int)(x & 31); }
+        // helper function for getting one bit from the secondary pattern buffer
+        uint GetBit(uint x, uint y) { return (second[y * pw + (x >> 5)] >> (int)(x & 31)) & 1U; }
         // mouse handling: dragging functionality
         uint xoffset = 0, yoffset = 0;
         bool lastLButtonState = false;
         int dragXStart, dragYStart, offsetXStart, offsetYStart;
 
-        long[] workSize = { 0, 0 };
-        
-
+        //COPYED FROM CPU VERSION
+        public void SetMouseState(int x, int y, bool pressed)
+        {
+            if (pressed)
+            {
+                if (lastLButtonState)
+                {
+                    int deltax = x - dragXStart, deltay = y - dragYStart;
+                    xoffset = (uint)Math.Min(pw * 32 - screen.width, Math.Max(0, offsetXStart - deltax));
+                    yoffset = (uint)Math.Min(ph - screen.height, Math.Max(0, offsetYStart - deltay));
+                }
+                else
+                {
+                    dragXStart = x;
+                    dragYStart = y;
+                    offsetXStart = (int)xoffset;
+                    offsetYStart = (int)yoffset;
+                    lastLButtonState = true;
+                }
+            }
+            else lastLButtonState = false;
+        }
 
         public void Init()
         {
-            StreamReader sr = new StreamReader("../../samples/turing_js_r.rle");
+            StreamReader sr = new StreamReader("C:/Users/Alijt Rijcken/Documents/Studie_2Bachelor/Concurrency_B2/GameOfLive - CPU/assets/turing_js_r.rle");
             uint state = 0, n = 0, x = 0, y = 0;
-            workSize[0] = screen.width; workSize[1] = screen.height;
             while (true)
             {
                 String line = sr.ReadLine();
@@ -75,6 +96,7 @@ namespace Template
                     }
             }
             // swap buffers
+<<<<<<< HEAD
             for (int i = 0; i < pw * ph; i++)
             {
                 second[i] = pattern[i];
@@ -83,6 +105,9 @@ namespace Template
 
             Sbuffer = new OpenCLBuffer<uint>(ocl, second);
             kernel.SetArgument(0, Sbuffer);
+=======
+            for (int i = 0; i < pw * ph; i++) second[i] = pattern[i];
+>>>>>>> parent of 1922a6c... iets gedaan hopelijk werkt het
             
             //informatie doorgeven naar de GPU - hele dure operatie. Je kan gaan files loaden op de GPU
             //Kopier de begin state één keer naar de GPU en daarna ga je de array's aanpassen. 
@@ -94,6 +119,7 @@ namespace Template
             // start timer
             timer.Restart();
             // run the simulation, 1 step
+<<<<<<< HEAD
             //Simulate();
             Sbuffer.CopyToDevice();
             kernel.Execute(workSize);
@@ -105,6 +131,9 @@ namespace Template
                 second[i] = second[i + second.Length / 2];
             }
 
+=======
+            Simulate();
+>>>>>>> parent of 1922a6c... iets gedaan hopelijk werkt het
             // visualize current state, DRAW FUNCTION -> GPU BONUS. 
             screen.Clear(0);
             for (uint y = 0; y < screen.height; y++)
@@ -116,75 +145,40 @@ namespace Template
 
         }
 
-        // helper function for setting one bit in the pattern buffer
-        void BitSet(uint x, uint y)
-        {
-            pattern[y * pw + (x >> 5)] |= 1U << (int)(x & 31);
-        }
-        // helper function for getting one bit from the secondary pattern buffer
-        uint GetBit(uint x, uint y)
-        {
-            return (second[y * pw + (x >> 5)] >> (int)(x & 31)) & 1U;
-        }
-
         // SIMULATE --> OpenCL Class ALLE BEREKENINGEN MOETEN NAAR DE GPU.
         // Takes the pattern in array 'second', and applies the rules of Game of Life to produce the next state
         // in array 'pattern'. At the end, the result is copied back to 'second' for the next generation.
-        //void Simulate()
-        //{
-        //    // clear destination pattern
-        //    for (int i = 0; i < pw * ph; i++) pattern[i] = 0;
-        //    // process all pixels, skipping one pixel boundary
-        //    uint w = pw * 32, h = ph;
-        //    for (uint y = 1; y < h - 1; y++) for (uint x = 1; x < w - 1; x++)
-        //        {
-        //            // count active neighbors
-        //            uint n = GetBit(x - 1, y - 1) + GetBit(x, y - 1) + GetBit(x + 1, y - 1) + GetBit(x - 1, y) +
-        //                GetBit(x + 1, y) + GetBit(x - 1, y + 1) + GetBit(x, y + 1) + GetBit(x + 1, y + 1);
-        //            if ((GetBit(x, y) == 1 && n == 2) || n == 3) BitSet(x, y);
-        //        }
-        //    // swap buffers
-        //    for (int i = 0; i < pw * ph; i++) second[i] = pattern[i];
-        //}
-
-        //public void Render()
-        //{
-        //    // use OpenGL to draw a quad using the texture that was filled by OpenCL
-        //    if (GLInterop)
-        //    {
-        //        GL.LoadIdentity();
-        //        GL.BindTexture( TextureTarget.Texture2D, image.OpenGLTextureID );
-        //        GL.Begin( PrimitiveType.Quads );
-        //        GL.TexCoord2( 0.0f, 1.0f ); GL.Vertex2( -1.0f, -1.0f );
-        //        GL.TexCoord2( 1.0f, 1.0f ); GL.Vertex2(  1.0f, -1.0f );
-        //        GL.TexCoord2( 1.0f, 0.0f ); GL.Vertex2(  1.0f,  1.0f );
-        //        GL.TexCoord2( 0.0f, 0.0f ); GL.Vertex2( -1.0f,  1.0f );
-        //        GL.End();
-        //    }
-        //}
-
-
-        //COPYED FROM CPU VERSION
-        public void SetMouseState(int x, int y, bool pressed)
+        void Simulate()
         {
-            if (pressed)
+            // clear destination pattern
+            for (int i = 0; i < pw * ph; i++) pattern[i] = 0;
+            // process all pixels, skipping one pixel boundary
+            uint w = pw * 32, h = ph;
+            for (uint y = 1; y < h - 1; y++) for (uint x = 1; x < w - 1; x++)
+                {
+                    // count active neighbors
+                    uint n = GetBit(x - 1, y - 1) + GetBit(x, y - 1) + GetBit(x + 1, y - 1) + GetBit(x - 1, y) +
+                        GetBit(x + 1, y) + GetBit(x - 1, y + 1) + GetBit(x, y + 1) + GetBit(x + 1, y + 1);
+                    if ((GetBit(x, y) == 1 && n == 2) || n == 3) BitSet(x, y);
+                }
+            // swap buffers
+            for (int i = 0; i < pw * ph; i++) second[i] = pattern[i];
+        }
+
+        public void Render()
+        {
+            // use OpenGL to draw a quad using the texture that was filled by OpenCL
+            if (GLInterop)
             {
-                if (lastLButtonState)
-                {
-                    int deltax = x - dragXStart, deltay = y - dragYStart;
-                    xoffset = (uint)Math.Min(pw * 32 - screen.width, Math.Max(0, offsetXStart - deltax));
-                    yoffset = (uint)Math.Min(ph - screen.height, Math.Max(0, offsetYStart - deltay));
-                }
-                else
-                {
-                    dragXStart = x;
-                    dragYStart = y;
-                    offsetXStart = (int)xoffset;
-                    offsetYStart = (int)yoffset;
-                    lastLButtonState = true;
-                }
+                GL.LoadIdentity();
+                GL.BindTexture( TextureTarget.Texture2D, image.OpenGLTextureID );
+                GL.Begin( PrimitiveType.Quads );
+                GL.TexCoord2( 0.0f, 1.0f ); GL.Vertex2( -1.0f, -1.0f );
+                GL.TexCoord2( 1.0f, 1.0f ); GL.Vertex2(  1.0f, -1.0f );
+                GL.TexCoord2( 1.0f, 0.0f ); GL.Vertex2(  1.0f,  1.0f );
+                GL.TexCoord2( 0.0f, 0.0f ); GL.Vertex2( -1.0f,  1.0f );
+                GL.End();
             }
-            else lastLButtonState = false;
         }
     } // class Game
 } // namespace Template
